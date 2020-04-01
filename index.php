0a7117f85bd6e9ccd7ec1b49d139d7283b5bec28
ljl22 <lauraj.libby@gmail.com>
2020-04-01 19:47:44 -0400

    Update image and coding for parallax scroll on index.html

--- index.php
+++ index.php
@@ -9,25 +9,28 @@
             <p>WP Carousel Plugin will go here</p>
         <!-- paralax scroll 1 here, hand code -->
 
+
+
                   <div class="parallax"></div>
 
                   <div style="height:300px; background-color:#0c005a;">
                     <h2 class="parallaxtext">Lorem Ipsum sentence here.</h2>
                   </div>
 
-        <!-----blog slider 2, here is the WP Carousel Plugin - will need to add as posts to Woo Commerce and then add to WP Carousel -->
+            <!-----blog slider 2, here is the WP Carousel Plugin - will need to add as posts to Woo Commerce and then add to WP Carousel -->
             <p>WP Carousel Plugin will go here</p>
+
 
         <!-- paralax scroll 2 here, hand code -->
 
-                    <div class="parallax"></div>
+                <!--    <div class="parallax"></div>
 
                     <div style="height:300px; background-color:#0c005a;" >
                       <h2 class="parallaxtext">Lorem Ipsum sentence here.</h2>
-                    </div>
+                    </div> -->
+          </content>
 
 
-</main>
 
 
 <div class="container">
