f3b20cbd50ef49e12bff3b9a7cb9e73cada2b6e7
ljl22 <lauraj.libby@gmail.com>
2020-03-31 21:22:36 -0400

    Update index and style to try and fix parallax scroll

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
 
-  <div class="container">
-    <div class="row">
 
+<main>
         <!--here is the smart slider plugin - hero image, install widget area, will need to be styled on remote site -->
 
         <!-- blog slider 1, here is the WP Carousel Plugin - will need to add as posts to Woo Commerce and then add to WP Carousel -->
@@ -11,10 +10,8 @@
 
                   <div class="parallax"></div>
 
-                  <div style="height:1000px; background-color:red;font-size:36px">
-                  Scroll Up and Down this page to see the parallax scrolling effect.
-                  This div is just here to enable scrolling.
-                  Tip: Try to remove the background-attachment property to remove the scrolling effect.
+                  <div style="height:500px; background-color:#0c005a;font-size:36px">
+                  Lorem Ipsum sentence here.
                   </div>
 
         <!-----blog slider 2, here is the WP Carousel Plugin - will need to add as posts to Woo Commerce and then add to WP Carousel -->
@@ -22,29 +19,32 @@
 
         <!-- paralax scroll 2 here, hand code -->
 
-                  <div class="parallax"></div>
+                    <div class="parallax"></div>
 
-                  <div style="height:1000px; background-color:red;font-size:36px">
-                  Scroll Up and Down this page to see the parallax scrolling effect.
-                  This div is just here to enable scrolling.
-                  Tip: Try to remove the background-attachment property to remove the scrolling effect.
-                  </div>
+                    <div style="height:500px; background-color:#0c005a;font-size:36px">
+                        Lorem Ipsum sentence here.
+                    </div>
 
 
+</main>
+                  <div class="container">
+                    <div class="row">
 <!--- previous code from assignment 3 starts------------------->
-      <?php  if(have_posts()){
+
+      <!--<?php  if(have_posts()){
         while(have_posts()){
           the_post(); ?>
           <div class="col-md-4 post-overview">
-            <h3><?php the_title(); ?></h3>
+            <h3><?php the_title(); ?></h3>-->
+
 
             <!-- Featured Image -->
-            <div class="post-featured-image">
+          <!--  <div class="post-featured-image">
               <?php the_post_thumbnail('medium'); ?>
-            </div>
+            </div> -->
 
             <!-- Post Information -->
-            <p class="post-info"><?php echo "Date: " . get_the_date(); echo " | "; echo "Author: " . get_the_author(); ?></p>
+          <!--  <p class="post-info"><?php echo "Date: " . get_the_date(); echo " | "; echo "Author: " . get_the_author(); ?></p>
 
             <?php the_excerpt(); ?>
 
@@ -52,7 +52,8 @@
           </div>
     <?php } //ends while loop
     } //ends if statement
-      ?>
+      ?> -->
+
 
 <!--- previous code from assignment 3 ends------------------->
 
