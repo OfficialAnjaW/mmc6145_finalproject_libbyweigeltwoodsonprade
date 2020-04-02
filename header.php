8b311e77d4170855bcbdee666f635199cebf2463
Anja Weigelt <aweigelt@ufl.edu>
2020-04-02 00:14:49 -0400

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <title><?php bloginfo('name'); ?></title>
 
@@ -12,7 +12,7 @@
     <?php wp_head(); ?>
 
     <!--Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,600,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,300&family=Oswald:wght@400;700&display=swap" rel="stylesheet">
   </head>
 
   <body>
