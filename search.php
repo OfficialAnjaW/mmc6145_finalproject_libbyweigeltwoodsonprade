542fa4053ac00b9641d1408977692a873b66af2a
Anja Weigelt <aweigelt@ufl.edu>
2020-04-05 16:56:30 -0400

    Make heading color updates

--- search.php
+++ search.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
-<h2>Search page currently under construction.</h2>
+<div class="container">
+  <div class="row">
+    <h2>Search page currently under construction.</h2>
+  </div>
+</div>
 
 <?php get_footer(); ?>
