a848eb1985c4597eb75797045fa5b574bb5d3da3
ljl22 <lauraj.libby@gmail.com>
2020-04-26 00:02:33 -0400

    Update about us page with featured img and content function

--- page-about.php
+++ page-about.php
@@ -10,7 +10,7 @@ Template Post Type: page
 
 
 <!-- ======================= Featured Image Function ==================-->
-    <?php the_post_thumbnail(); ?>
+  <?php the_post_thumbnail(); ?>
 
   <!--======================= start about us content post=====================------->
     <section>
